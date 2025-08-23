e7bd299304c1de08e4ce7418e32d6406ef4966ef
Shahriar262 <srh.2622@gmail.com>
2025-08-23 20:17:32 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//login button functionality
+
+document.getElementById('btn-login').addEventListener('click', function(e){
+    e.preventDefault();
+    const mobileNum = 12345678910;
+    const pinNum = 1234;
+    const mobileNumValue = document.getElementById('mobile-num').value;
+    const mobileNumValueConverted = parseInt(mobileNumValue);
+
+    const pinNumValue = document.getElementById('pin-num').value;
+    const pinNumValueConverted = parseInt(pinNumValue)
+   
+    if(mobileNumValueConverted === mobileNum && pinNumValueConverted === pinNum){
+        window.location.href="./home.html";
+        
+    }
+    else{
+        alert('Invalid credentials');
+        
+    }
+    
+})
