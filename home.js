dcd9a4a6d48fab55c5c0097397577676d968e8a3
Shahriar262 <srh.2622@gmail.com>
2025-08-26 02:49:54 +0600

    added cash-out section

--- home.js
+++ home.js
@@ -1,12 +1,30 @@
 const validPin = 1234
 
+//function to get input values
+function getInputValueNum(id){
+    const value = document.getElementById(id).value.trim();
+    const num = parseInt(value);
+
+    if(isNaN(num) || num.toString() !== value){
+        return NaN; 
+    }
+    return num;
+}
+
+//get inner text function call
+  function getInnerText(id){
+    return parseInt(document.getElementById('available-balance').innerText);
+  }
+
+
+// add-money feature
 document.getElementById('btn-add-money').addEventListener('click', function(e){
  e.preventDefault();
  const bank = document.getElementById('bank').value;
  const accountNum = document.getElementById('bank-acc-num').value;
- const amount = parseInt(document.getElementById('bank-amount').value);
- const pinNum = parseInt(document.getElementById('pin-num').value);
- const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+ const amount = getInputValueNum('bank-amount');
+ const pinNum = getInputValueNum('pin-num');
+ const availableBalance = getInnerText('available-balance');
 
  if(accountNum.length < 11){
     alert('Please provide valid account number');
@@ -18,18 +36,65 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
     return;
  }
 
+ if(isNaN(amount) || amount <= 0){
+   alert('Please enter a valid amount');
+   return;
+}
+
+
  const totalNewBalance = amount + availableBalance;
  document.getElementById('available-balance').innerText = totalNewBalance;
 });
 
+// cash out feature
+const cashOutPin = 1234
 
+document.getElementById('btn-withdraw-money').addEventListener('click', function(e){
+   e.preventDefault();
+   const agentNum = document.getElementById('agent-num').value;
+   const withdrawAmount = getInputValueNum('withdraw-amount');
+   const cashOutPinNum = getInputValueNum('cash-out-pin');
+   const availableBalance = getInnerText('available-balance');
+
+   if(agentNum.length < 11){
+      alert('Please provide valid Agent Number')
+      return;
+   }
+
+   if(cashOutPinNum !== cashOutPin){
+      alert('Please provide correct pin')
+      return;
+   }
+
+   if(isNaN(withdrawAmount) || withdrawAmount <= 0){
+   alert('Please enter a valid Withdraw amount');
+   return;
+}
+
+if(withdrawAmount > availableBalance){
+   alert('Insufficient balance');
+   return;
+}
+
+
+
+   const newBalance = availableBalance - withdrawAmount;
+   document.getElementById('available-balance').innerText = newBalance;
+})
 // toggling feature
 
 document.getElementById('add-btn').addEventListener('click', function(){
    document.getElementById('cash-out-parent').style.display = 'none';
+   document.getElementById('transfer-parent').style.display = 'none'
    document.getElementById('add-money-parent').style.display = 'block';
 })
 document.getElementById('cash-out-btn').addEventListener('click', function(){
    document.getElementById('add-money-parent').style.display = 'none';
+   document.getElementById('transfer-parent').style.display = 'none'
    document.getElementById('cash-out-parent').style.display = 'block';
+})
+document.getElementById('transfer-btn').addEventListener('click', function(){
+   document.getElementById('add-money-parent').style.display = 'none';
+   document.getElementById('cash-out-parent').style.display = 'none';
+   document.getElementById('transfer-parent').style.display = 'block';
 })
